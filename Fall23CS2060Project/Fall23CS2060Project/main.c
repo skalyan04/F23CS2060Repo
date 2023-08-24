c75a08d5096a85ddacdd887914a9589738f6585b
skalyan04 <123116625+skalyan04@users.noreply.github.com>
2023-08-24 07:39:59 -0600

    Add files via upload

---
+++ Fall23CS2060Project/Fall23CS2060Project/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Fall23CS2060Project
+//
+//  Created by Sreya Kalyan on 8/24/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
